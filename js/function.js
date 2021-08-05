e0ee70aec317ac258cb935a08f3d87bb0cd1d88f
Juan David R <jdavidreyes09@gmail.com>
2021-08-05 17:39:13 -0500

    Avance header y parte del hero

---
+++ js/function.js
@@ -0,0 +1,6 @@
+const swiper = new Swiper(".Swiper_blog_hero", {
+    navigation: {
+      nextEl: ".Swiper_blog_hero .swiper-button-next",
+      prevEl: ".Swiper_blog_hero .swiper-button-prev",
+    },
+  });
