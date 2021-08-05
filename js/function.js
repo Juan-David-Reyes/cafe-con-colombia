2deb3a1e38d47f8105917abbac1cf6df8b96929b
Juan David R <jdavidreyes09@gmail.com>
2021-08-05 18:23:28 -0500

    hero section avance

--- js/function.js
+++ js/function.js
@@ -1,4 +1,5 @@
 const swiper = new Swiper(".Swiper_blog_hero", {
+    parallax: true,
     navigation: {
       nextEl: ".Swiper_blog_hero .swiper-button-next",
       prevEl: ".Swiper_blog_hero .swiper-button-prev",
