032e3df39c85932a5eab10e29b505bfb0d740921
Juan David R <jdavidreyes09@gmail.com>
2021-08-06 19:23:16 -0500

    Home Blog finalizado y responsive OK

--- js/function.js
+++ js/function.js
@@ -1,9 +1,20 @@
 const swiper = new Swiper(".Swiper_blog_hero", {
     effect: 'fade',
-
     parallax: true,
     navigation: {
       nextEl: ".Swiper_blog_hero .swiper-button-next",
       prevEl: ".Swiper_blog_hero .swiper-button-prev",
     },
+  });
+
+  const btnSearch = document.querySelector('a.search');
+  const closeMain = document.querySelector('main');
+  const inputSearch = document.querySelector('.cont_search');
+
+  btnSearch.addEventListener('click', () => {
+    inputSearch.classList.toggle('active_search');
+  });
+
+  closeMain.addEventListener('click', () => {
+    inputSearch.classList.remove('active_search');
   });
