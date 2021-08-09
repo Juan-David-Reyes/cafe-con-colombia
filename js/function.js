a2d58e4ad70a2e7922705d3cab5236abfa31f3d7
Juan David R <jdavidreyes09@gmail.com>
2021-08-08 19:48:11 -0500

    avance nota

--- js/function.js
+++ js/function.js
@@ -11,10 +11,22 @@ const swiper = new Swiper(".Swiper_blog_hero", {
   const closeMain = document.querySelector('main');
   const inputSearch = document.querySelector('.cont_search');
 
+  // MOBILE
+
+  const btnSearchMobile = document.querySelector('.navbar_MV a.search');
+  const inputSearchMobile = document.querySelector('.navbar_MV .cont_search');
+
   btnSearch.addEventListener('click', () => {
     inputSearch.classList.toggle('active_search');
   });
 
   closeMain.addEventListener('click', () => {
     inputSearch.classList.remove('active_search');
+    inputSearchMobile.classList.remove('active_search');
+  });
+
+  // MOBILE
+
+  btnSearchMobile.addEventListener('click', () => {
+    inputSearchMobile.classList.toggle('active_search');
   });
